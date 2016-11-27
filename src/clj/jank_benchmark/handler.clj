28eef339611fa837ee999078f0614e44eaef428e
jeaye <contact@jeaye.com>
2016-11-26 20:17:58 -0800

    Add a /stats server route to return data

--- src/clj/jank_benchmark/handler.clj
+++ src/clj/jank_benchmark/handler.clj
@@ -26,9 +26,19 @@
      mount-target
      (include-js "/js/app.js")]))
 
+(def data [{:name "Jan 1" :uv 4000 :pv 2400 :amt 2400}
+           {:name "Feb 1" :uv 3000 :pv 1398 :amt 2210}
+           {:name "Mar 1" :uv 2000 :pv 9800 :amt 2290}
+           {:name "Apr 1" :uv 2780 :pv 3908 :amt 2000}
+           {:name "May 1" :uv 1890 :pv 4800 :amt 2181}
+           {:name "Jun 1" :uv 2390 :pv 3800 :amt 2500}
+           {:name "Jul 1" :uv 3490 :pv 4300 :amt 2100}
+           {:name "Aug 1" :uv 6490 :pv 5300 :amt 2100}])
+
 (defroutes app-routes
   (GET "/" [] (loading-page))
   (GET "/about" [] (loading-page))
+  (GET "/stats" [] data)
   (resources "/")
   (not-found "Not Found"))
 
