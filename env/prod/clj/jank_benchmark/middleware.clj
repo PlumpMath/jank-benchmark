c30a04a14b3fc8fa5ab7b6fe35800bc73cd0e7ea
jeaye <contact@jeaye.com>
2016-11-26 20:10:52 -0800

    Expand on security for prod environment

--- env/prod/clj/jank_benchmark/middleware.clj
+++ env/prod/clj/jank_benchmark/middleware.clj
@@ -1,5 +1,15 @@
 (ns jank-benchmark.middleware
-  (:require [ring.middleware.defaults :refer [secure-site-defaults wrap-defaults]]))
+  (:require [ring.middleware
+             [defaults :refer [secure-site-defaults wrap-defaults]]
+             [gzip :as gzip]]))
 
 (defn wrap-middleware [handler]
-  (wrap-defaults handler secure-site-defaults))
+  (-> handler
+      (wrap-defaults (-> secure-site-defaults
+                         (assoc :cookies false)
+                         (assoc :session {})
+                         (assoc :proxy true)
+                         (assoc-in [:static :resources] "/")
+                         (assoc-in [:security :hsts] false)
+                         (assoc-in [:security :ssl-redirect] false)))
+      gzip/wrap-gzip))
