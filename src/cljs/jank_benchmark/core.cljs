510a6b0f0cdbce363db690e29bf12fa5e6721d11
jeaye <contact@jeaye.com>
2016-11-26 20:53:20 -0800

    Hook client data up to a ratom

--- src/cljs/jank_benchmark/core.cljs
+++ src/cljs/jank_benchmark/core.cljs
@@ -12,20 +12,13 @@
 ;; -------------------------
 ;; Views
 
-(def data [{:name "Jan 1" :uv 4000 :pv 2400 :amt 2400}
-           {:name "Feb 1" :uv 3000 :pv 1398 :amt 2210}
-           {:name "Mar 1" :uv 2000 :pv 9800 :amt 2290}
-           {:name "Apr 1" :uv 2780 :pv 3908 :amt 2000}
-           {:name "May 1" :uv 1890 :pv 4800 :amt 2181}
-           {:name "Jun 1" :uv 2390 :pv 3800 :amt 2500}
-           {:name "Jul 1" :uv 3490 :pv 4300 :amt 2100}
-           {:name "Aug 1" :uv 6490 :pv 5300 :amt 2100}])
+(def data (reagent/atom {}))
 
 (defn home-page []
   [:> js/Recharts.LineChart {:width 1000
                              :height 700
                              :margin {:top 5, :right 30, :left 20, :bottom 5}
-                             :data data}
+                             :data @data}
    [:> js/Recharts.XAxis {:dataKey "name"}]
    [:> js/Recharts.YAxis]
    [:> js/Recharts.CartesianGrid {:strokeDasharray "3 3"}]
@@ -68,7 +61,7 @@
           reply (-> (js/JSON.parse (:body reply-js))
                     js->clj
                     keywordify)]
-      (pprint reply))))
+      (reset! data reply))))
 
 (defn init! []
   (accountant/configure-navigation!
